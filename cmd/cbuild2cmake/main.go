9e4ed9ea70e217919ff215d2aa236fe11fe19231
Daniel Brondani <daniel.brondani@arm.com>
2024-07-19 09:47:59 +0200

    [cbuild2cmake] Redirect error messages to `stderr`

--- cmd/cbuild2cmake/main.go
+++ cmd/cbuild2cmake/main.go
@@ -8,16 +8,19 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/Open-CMSIS-Pack/cbuild2cmake/cmd/cbuild2cmake/commands"
 
 	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus/hooks/writer"
 )
 
 func main() {
 	log.SetFormatter(new(LogFormatter))
-	log.SetOutput(os.Stdout)
+	log.SetOutput(io.Discard)
+	initLogHooks()
 
 	if len(version) == 0 {
 		version = "0.0.0-debug"
@@ -38,4 +41,23 @@ type LogFormatter struct{}
 func (s *LogFormatter) Format(entry *log.Entry) ([]byte, error) {
 	msg := fmt.Sprintf("%s cbuild2cmake: %s\n", entry.Level.String(), entry.Message)
 	return []byte(msg), nil
+}
+
+func initLogHooks() {
+	log.AddHook(&writer.Hook{ // Send logs with level higher than warning to stderr
+		Writer: os.Stderr,
+		LogLevels: []log.Level{
+			log.PanicLevel,
+			log.FatalLevel,
+			log.ErrorLevel,
+			log.WarnLevel,
+		},
+	})
+	log.AddHook(&writer.Hook{ // Send info and debug logs to stdout
+		Writer: os.Stdout,
+		LogLevels: []log.Level{
+			log.InfoLevel,
+			log.DebugLevel,
+		},
+	})
 }
